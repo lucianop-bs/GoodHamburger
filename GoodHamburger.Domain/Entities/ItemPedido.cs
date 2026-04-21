0a631e3a25cc4950bb894d410455021afd7b4df1
Luciano Borges <lucianop.borges1@icloud.com>
2026-04-20 21:04:34 -0300

    feat: implementa dominio e suas propriedades

---
+++ GoodHamburger.Domain/Entities/ItemPedido.cs
@@ -0,0 +1,22 @@
+﻿using GoodHamburger.Domain.Enums;
+
+namespace GoodHamburger.Domain.Entities
+{
+    public class ItemPedido
+    {
+
+        public Guid Id { get; private set; }
+        public Guid ProdutoId { get; private set; }
+        public Categoria Categoria { get; private set; }
+        public decimal PrecoUnitario { get; private set; }
+
+        public ItemPedido(Guid produtoId, Categoria categoria, decimal precoUnitario)
+        {
+            Id = Guid.NewGuid();
+            ProdutoId = produtoId;
+            Categoria = categoria;
+            PrecoUnitario = precoUnitario;
+        }
+
+    }
+}
---
+++ GoodHamburger.Domain/Enums/Categoria.cs
@@ -0,0 +1,9 @@
+﻿namespace GoodHamburger.Domain.Enums
+{
+    public enum Categoria
+    {
+        Sanduiche = 1,
+        Bebida = 2,
+        Acompanhamento = 3
+    }
+}
